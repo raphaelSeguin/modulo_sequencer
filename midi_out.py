fc16313fee5b715e5b71c8e3df351b360d6a08f2
Raphael Seguin <raphaelseguin@gmail.com>
2022-08-12 11:38:35 +0200

    midi out

---
+++ midi_out.py
@@ -0,0 +1,18 @@
+from pyo import *
+
+s = Server()
+s.deactivateMidi()
+s.boot()
+
+dispatch = MidiDispatcher(4)
+dispatch.start()
+
+def ping():
+    global dispatch
+    dispatch.send(144, 60, 127)
+
+pat = Pattern(function=ping, time=0.1)
+pat.play()
+
+s.start()
+s.gui(locals())

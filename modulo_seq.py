edd2314df38edd44f7c7be7e3d0a6981c3d5194e
Raphael Seguin <raphaelseguin@gmail.com>
2022-08-19 15:10:01 +0200

    modulo seq v2

--- modulo_seq.py
+++ modulo_seq.py
@@ -2,11 +2,9 @@ import sys
 from pyo import * 
 import wx
 
-from percs import Kick, Snare, HiHat
+from percs import Kick, Snare, HiHat, CowBell, Tom, Cym
 
-# MUTE
-# 4 modulos
-# samples
+# serialize -> yml -> restore
 
 server = Server().boot().start()
 
@@ -17,28 +15,65 @@ class Interface(wx.Frame):
         wx.Frame.__init__(self, None, id=-1, title="I", pos=(20, 50), size=(1200, 600))
         colWidth = 50
         linHeight = 25
-        instLabels = ('k', 'sn', 'hh', 'cb')
+        cols = 8
+        self.patternChoiceA = wx.Choice(self, id=wx.ID_ANY, pos=(80 + 8*colWidth, 0), size=wx.DefaultSize,
+            choices=[str(n) for n in range(10)], style=0, name="")
+        self.patternChoiceB = wx.Choice(self, id=wx.ID_ANY, pos=(80 + 9*colWidth, 0), size=wx.DefaultSize,
+            choices=[str(n) for n in range(10)], style=0, name="")
+        self.patternCrossfader = wx.Slider(self, id=wx.ID_ANY, value=20, minValue=0, maxValue=100,
+                pos=(80 + 8*colWidth, 25), size=(80, 20), style=wx.SL_HORIZONTAL,
+                name="")
+        instLabels = ('k', 'sn', 'hh', 'cb', 'ht', 'mt', 'lt', 'cym')
         self.instLables = [
             wx.StaticText(self, id=n, label=instLabels[n], pos=(80 + n * colWidth, 0), size=(50, 20), style=0, name="")
             for n, label in enumerate(instLabels)
         ]
-        labels = ('mod1', 'mod2', 'mod3', 'offset', 'min', 'max', '_', '_')
+        labels = ('mod1', 'mod2', 'mod3', 'mod4', 'offset', 'mmod1', 'mmod2', 'mlength', 'moffset', 'mute', 'faders')
+        defaultValues = ( 32, 16, 8, 4, 0, 16, 16, 16, 0 )
         self.NumbersLabels = [ 
             wx.StaticText(self, id=n, label=labels[n], pos=(0, 50 + n*linHeight), 
-            size=(50, 20), style=0, name="") for n in range(8)
+                size=(50, 20), style=0, name="")
+            for n in range(len(labels))
         ]
         self.numbers = [ 
-            wx.SpinCtrl(self, id=n+x*10, value="pouet", pos=(80 + x*colWidth, 50 + n*linHeight),
-               size=(colWidth, 25), style=wx.SP_ARROW_KEYS, min=1, max=64, initial=8,
-               name="wxSpinCtrlDouble") 
-            for n in range(8)
-            for x in range(4)
+            wx.SpinCtrl(self, 
+                id=row+col*10,
+                pos=(80 + col*colWidth, 50 + row*linHeight),
+                size=(colWidth, 25),
+                style=wx.SP_ARROW_KEYS,
+                min=0,
+                max=64,
+                initial=defaultValues[row],
+                name="wxSpinCtrlDouble_{row}_{col}".format(row=row, col=col)) 
+            for row in range(9)
+            for col in range(cols)
         ]
         [ number.Bind(wx.EVT_SPINCTRL, self.handleNumber) for number in self.numbers ]
+        self.muteButtons = [
+            wx.ToggleButton(
+                self,
+                id=7+(col+1)*100,
+                label="",
+                pos=(80 + col*colWidth, 50 + 9*linHeight),
+                size=(50, 20),
+            )
+            for col in range(cols)
+        ]
+        [ muteButton.SetValue(1) for muteButton in self.muteButtons ]
+        [ muteButton.Bind(wx.EVT_TOGGLEBUTTON, self.handleMuteButtons) for muteButton in self.muteButtons ]
+        self.faders = [
+            wx.Slider(self, id=wx.ID_ANY, value=20, minValue=0, maxValue=100,
+                pos=(80 + col*colWidth, 50 + 10*linHeight), size=(colWidth, 100), style=wx.SL_VERTICAL,
+                name="")
+            for col in range(cols)
+        ]
         self.callbacks = {}
 
     def handleNumber(self, event):
         self.dispatch(event.GetId(), event.GetPosition())
+    
+    def handleMuteButtons(self, event):
+        self.dispatch(event.GetId(), bool(event.GetInt()))
     
     def dispatch(self, id, value):
         if id in self.callbacks:
@@ -50,12 +85,12 @@ class Interface(wx.Frame):
 
 class ModuloSequencer():
     def __init__(self):
-        self.count = 0
+        self.modulos = [32, 16, 8, 4]
         self.offset = 0
-        self.modulos = [ 16 ] * 3
-    
-    def tick(self):
-        self.count += 1
+        self.maskModulos = [16, 16]
+        self.maskLength = 16
+        self.maskOffset = 0
+        self.muted = True
 
     def setModulos(self, n, value):
         self.modulos[n] = value
@@ -63,50 +98,134 @@ class ModuloSequencer():
     def setOffset(self, value):
         self.offset = value
 
+    def setMaskModulos(self, n, value):
+        print("setMaskModulos", n, value)
+        self.maskModulos[n] = value
+
+    def setMaskLength(self, value):
+        print("setMaskLength", value)
+        self.maskLength = value
+
+    def setMaskOffset(self, value):
+        print("setMaskOffset", value)
+        self.maskOffset = value
+
+    def setStep(self, value):
+        self.step = value
+    
+    def setMuted(self, value):
+        print("muted", value)
+        self.muted = value
+
     @property
     def value(self):
-        result = self.count + self.offset
+        result = self.step + self.offset
         for modulo in self.modulos:
-            result = result % modulo
+            result %= modulo
         return result
+
+    @property
+    def mask(self):
+        maskStep = self.step + self.maskOffset
+        for modulo in self.maskModulos:
+            maskStep %= modulo
+        return maskStep < self.maskLength
+
+    @property
+    def trigger(self):
+        if self.value == 0 and self.mask and not self.muted:
+            return True
+        else:
+            return False
+
+class Clock():
+    def __init__(self, bpm=150, divisor=4):
+        time = 60 / (bpm * divisor)
+        self.metro = Metro(time=time)
+        self.count = 0
+        
+    def play(self):
+        self.metro.play()
+
+    def bind(self, function):
+        self.callback = function
+        self.trig = TrigFunc(self.metro, self.tick)
+
+    def unbind(self):
+        self.callback = None
+
+    def tick(self):
+        self.count += 1
+        if self.callback:
+            self.callback(self.count)
+
 
 class Controller():
     def __init__(self):
-        self.interface = interface = Interface()
+        self.interface = Interface()
         self.interface.Show()
-        self.sequencers = [ ModuloSequencer() for n in range(3) ]
+        sequencersNumber = 8
+        self.sequencers = [ ModuloSequencer() for n in range(sequencersNumber) ]
+        [ self.interface.bind(0 + num + seq*10, self.makeModuloSetter(seq, num)) for num in range(4) for seq in range(sequencersNumber) ]
+        [ self.interface.bind(4 + seq*10, self.makeOffsetSetter(seq)) for seq in range(sequencersNumber) ]
+        [ self.interface.bind(5 + num + seq*10, self.makeMaskModuloSetter(seq, num)) for num in range(2) for seq in range(sequencersNumber) ]
+        [ self.interface.bind(7 + seq*10, self.makeMaskLengthSetter(seq)) for seq in range(sequencersNumber) ]
+        [ self.interface.bind(8 + seq*10, self.makeMaskOffsetSetter(seq)) for seq in range(sequencersNumber) ]
+        [ self.interface.bind(7 + seq*100, self.makeMuteSetter(seq)) for seq in range(1, sequencersNumber+1) ]
+#
+#     ! ! ! ! !
+#
+        # for seq, button in enumerate(self.interface.muteButtons):
+        #     print(button)
+        #     button.Bind(wx.EVT_TOGGLEBUTTON, self.makeMuteSetter(seq))
+#
 
-        [ self.interface.bind(num + seq*10, self.makeModuloSetter(seq, num)) for num in range(3) for seq in range(4) ]
+#
+        self.sounds = [ Kick(), Snare(), HiHat(), CowBell(), Tom(500), Tom(400), Tom(200), Cym() ]
 
-        self.interface.bind(3, self.offsetSetter0)
-        self.interface.bind(13, self.offsetSetter10)
-
-        self.sounds = [ Kick(), Snare(), HiHat() ]
-
-        self.clock = Metro(time=0.1)
+        self.clock = Clock(bpm=150, divisor=4)
         self.clock.play()
-        self.trigfunc = TrigFunc(self.clock, self.periodic)
-
+        self.clock.bind(self.periodic)
     # modulos
     def makeModuloSetter(self, sequencer, moduloNumber):
         def moduloSetter(value):
             self.sequencers[sequencer].setModulos(moduloNumber, value)
         return moduloSetter
-
     # offsets
-    def offsetSetter0(self, value):
-        self.sequencers[0].setOffset(value)
+    def makeOffsetSetter(self, sequencer):
+        def offsetSetter(value):
+            self.sequencers[sequencer].setOffset(value)
+        return offsetSetter
+    # maskModulos
+    def makeMaskModuloSetter(self, sequencer, moduloNumber):
+        def maskModuloSetter(value):
+            self.sequencers[sequencer].setMaskModulos(moduloNumber, value)
+        return maskModuloSetter
+    # maskLength
+    def makeMaskLengthSetter(self, sequencer):
+        def maskLengthSetter(value):
+            self.sequencers[sequencer].setMaskLength(value)
+        return maskLengthSetter
+    # maskOffset
+    def makeMaskOffsetSetter(self, sequencer):
+        def maskOffsetSetter(value):
+            self.sequencers[sequencer].setMaskOffset(value)
+        return maskOffsetSetter
+    # mutes
+    def makeMuteSetter(self, sequencer):
+        def muteSetter(value):
+            # print(sequencer, value)
+            self.sequencers[sequencer].setMuted(value)
+        return muteSetter
 
-    def offsetSetter10(self, value):
-        self.sequencers[1].setOffset(value)
-
-    def periodic(self):
+    def periodic(self, count):
+        # self.interface.stepDisplay.SetValue(str(count))
         for i, sequencer in enumerate(self.sequencers): 
-            sequencer.tick()
-            if sequencer.value == 0:
+            sequencer.setStep(count)
+            if sequencer.trigger:
                 self.sounds[i].play()
 
-
 controller = Controller()
+
 app.MainLoop()
 sys.exit()
--- percs.py
+++ percs.py
@@ -12,10 +12,47 @@ class Kick:
 
 class Snare:
     def __init__(self):
-        self.amplitudeEnvelope = Expseg([(0, 1), (0.095, 1), (0.1, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=1, add=0)
+        self.amplitudeEnvelope = Expseg([(0, 1), (0.5, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=0.8, add=0)
 
     def play(self):
         self.noise = PinkNoise(mul=self.amplitudeEnvelope)
         self.decim = Degrade(self.noise, bitdepth=4, srscale=0.1, mul=1, add=0)
         self.filter = Biquad(self.decim, freq=200, q=1, type=1, mul=1, add=0).mix(2).out()
+        self.amplitudeEnvelope.play()
+
+class HiHat:
+    def __init__(self):
+        self.amplitudeEnvelope = Expseg([(0, 1), (0.2, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=1, add=0)
+
+    def play(self):
+        self.noise = Noise(mul=self.amplitudeEnvelope)
+        self.filter = Biquad(self.noise, freq=8000, q=1, type=1, mul=1, add=0).mix(2).out()
+        self.amplitudeEnvelope.play()
+
+class CowBell:
+    def __init__(self):
+        self.amplitudeEnvelope = Expseg([(0, 1), (0.2, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=1, add=0)
+
+    def play(self):
+        self.fm = FM(carrier=1500, ratio=5/7, index=2., mul=self.amplitudeEnvelope).mix(2).out()
+        self.amplitudeEnvelope.play()
+
+class Tom():
+    def __init__(self, pitch):
+        self.amplitudeEnvelope = Expseg([(0, 1), (0.5, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=1, add=0)
+        self.pitchEnvelope = Expseg([(0, pitch * 3), (0.03, pitch), (0.5, pitch*0.9)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=1, add=0)
+        self.timbreEnvelope = Expseg([(0, 1), (0.03, 0.5), (0.5, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False)
+    def play(self):
+        self.oscillator = RCOsc(freq=self.pitchEnvelope, sharp=self.timbreEnvelope ,mul=self.amplitudeEnvelope).mix(2).out()
+        self.pitchEnvelope.play()
+        self.amplitudeEnvelope.play()
+        self.timbreEnvelope.play()
+
+class Cym():
+    def __init__(self):
+        self.amplitudeEnvelope = Expseg([(0, 1), (2, 0)], loop=False, exp=10, inverse=True, initToFirstVal=False, mul=1, add=0)
+    
+    def play(self):
+        self.noise = Noise(mul=self.amplitudeEnvelope)
+        self.filter = Biquad(self.noise, freq=7000, q=1, type=1, mul=1, add=0).mix(2).out()
         self.amplitudeEnvelope.play()
